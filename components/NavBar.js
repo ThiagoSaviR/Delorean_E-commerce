3112f826cbde39103b2819336359044f2f653639
ThiagoSaviR <thiagosavir@gmail.com>
2022-01-10 09:40:12 -0300

    feature: cria permanencia dos dados do carrinho

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,30 +3,39 @@ import Link from "next/link";
 import logo from "../public/store.png";
 import { useCart } from "../contexts/cartContext";
 
-
 const NavBar = () => {
-  const cart = useCart()
-  
-  const itemsCount = Object.keys(cart.cart).length;
+  const cart = useCart();
 
+  const itemsCount = Object.keys(cart.cart).length;
 
   return (
     <nav className="navbar">
       <div className="logo">
         <Link href={"/"}>
-        <Image src={logo} alt="Logo da Loja" />
+          <a>
+            <Image src={logo} alt="Logo da Loja" />
+          </a>
         </Link>
       </div>
       <div className="menu">
-          <ul>
-            <li><a href={`/section/consoles`}>Consoles</a></li>
-            <li><a href={`/section/jogos`}>Jogos</a></li>
-            <li><a href={`/section/acessorios`}>Acessórios</a></li>
-            <li><a href={`/section/carrinho`}>Carrinho
-            { itemsCount > 0 && <span className="num-cart">{itemsCount}</span>}
-            </a></li>
-          </ul>
-        </div>
+        <ul>
+          <li>
+            <a href={`/section/consoles`}>Consoles</a>
+          </li>
+          <li>
+            <a href={`/section/jogos`}>Jogos</a>
+          </li>
+          <li>
+            <a href={`/section/acessorios`}>Acessórios</a>
+          </li>
+          <li>
+            <a href={`/section/carrinho`}>
+              Carrinho
+              {itemsCount > 0 && <span className="num-cart">{itemsCount}</span>}
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
--- contexts/cartContext.js
+++ contexts/cartContext.js
@@ -1,15 +1,26 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useState } from "react";
 
 export const Cartcontext = createContext({});
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  useEffect(() => {
+    const localCart = window.localStorage.getItem('cart')
+    if(localCart){
+      setCart(JSON.parse(localCart))
+    }
+  }, [])
   const addToCart = (product) => {
-    setCart((old) => ({
-      ...old,
-      [product.id]: product,
-    }));
+   
+    setCart((old) => {
+      const newCart = {
+        ...old,
+        [product.id]: product,
+      }
+      window.localStorage.setItem('cart', JSON.stringify(newCart))
+      return newCart
+    });
   };
   return (
     <Cartcontext.Provider value={{ cart, addToCart }}>
